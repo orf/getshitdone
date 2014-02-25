86555e6cd25b64f5c20960417df98ad81ecac0be
orf <tom@tomforb.es>
2014-02-25 19:23:15 +0000

    Share icons now trigger GA events

--- getshitdone.py
+++ getshitdone.py
@@ -16,6 +16,7 @@ app.secret_key = "Z<\xe7`W\x03\xda\xd5p\x8ab\xfe\x05O\x00\xcc\xa2\xf9\x04e\xfeSg
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://getshitdone:password@localhost:5432/getshitdone'
 db = SQLAlchemy(app)
 CsrfProtect(app)
+session.permanent = True
 
 ADMIN_PASSWORD = "Qw9EN3nd"
 
